931309c409178687b51851c02f7e2f18e3e1505d
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-08 20:46:37 +0530

    Create user.js

---
+++ working_with_mvc/Models/user.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+const UserSchema=new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    }
+})
+const UserModel=mongoose.model('user',UserSchema);
+module.exports=UserModel;
